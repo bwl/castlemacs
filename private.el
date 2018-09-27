398a28fbaa8ffdfd66773bfe5590dc57b1179912
bwl <benlundquist@gmail.com>
2018-09-26 22:32:13 -0400

    Adding .org font config - header sizes and non fixed-width font

--- private.el
+++ private.el
@@ -37,3 +37,36 @@
 (set-cursor-color "#ffffff")
 
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+
+(use-package org-bullets
+  :after org
+  :hook
+  (org-mode . (lambda () (org-bullets-mode 1))))
+
+
+(let* ((variable-tuple
+        (cond ((x-list-fonts   "Source Sans Pro") '(:font   "Source Sans Pro"))
+              ((x-list-fonts   "Lucida Grande")   '(:font   "Lucida Grande"))
+              ((x-list-fonts   "Verdana")         '(:font   "Verdana"))
+              ((x-family-fonts "Sans Serif")      '(:family "Sans Serif"))
+              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+       (base-font-color (face-foreground 'default nil 'default))
+       (headline       `(:inherit default :weight bold :foreground ,base-font-color)))
+
+  (custom-theme-set-faces
+   'user
+   `(variable-pitch     ((t (:family "Source Sans Pro" :height 160 :weight light))))
+   `(fixed-pitch        ((t (:family "Source Code Pro"))))
+   `(org-level-8        ((t (,@headline ,@variable-tuple))))
+   `(org-level-7        ((t (,@headline ,@variable-tuple))))
+   `(org-level-6        ((t (,@headline ,@variable-tuple))))
+   `(org-level-5        ((t (,@headline ,@variable-tuple))))
+   `(org-level-4        ((t (,@headline ,@variable-tuple :height 1))))
+   `(org-level-3        ((t (,@headline ,@variable-tuple :height 1))))
+   `(org-level-2        ((t (,@headline ,@variable-tuple :height 1.10))))
+   `(org-level-1        ((t (,@headline ,@variable-tuple :height 1.25))))
+   `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
+
+
+(add-hook 'org-mode-hook 'visual-line-mode)
+(add-hook 'org-mode-hook 'variable-pitch-mode)
